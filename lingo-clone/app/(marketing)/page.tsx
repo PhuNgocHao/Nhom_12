6a4b192ae4bbaf6e690fb2840cdcb346af258ec2
QuangTN205 <23010860@st.phenikaa-uni.edu.vn>
2025-05-29 16:06:50 +0000

    Thêm các trang Marketing và thiết lập Shadcn UI

---
+++ lingo-clone/app/(marketing)/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return(
+    <div>
+      Day la trang marketing
+    </div>
+  );
+};
--- lingo-clone/app/layout.tsx
+++ lingo-clone/app/layout.tsx
@@ -1,14 +1,9 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Nunito } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
+const font = Nunito({
   variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
   subsets: ["latin"],
 });
 
@@ -23,12 +18,11 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
-    </html>
+      <html lang="en">
+        <body
+          className={font.variable}>
+          {children}
+        </body>
+      </html>
   );
 }
