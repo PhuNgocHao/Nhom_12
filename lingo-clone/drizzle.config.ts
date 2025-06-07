35da4bbe9c97d66f3b6ed26c2e316e7c838f12f0
PhuNgocHao <151693542+PhuNgocHao@users.noreply.github.com>
2025-06-07 14:52:34 +0000

    Closes #77: Vô địch rồi ?

---
+++ lingo-clone/drizzle.config.ts
@@ -0,0 +1,11 @@
+import "dotenv/config";
+import type { defineConfig } from "drizzle-kit";
+
+export default {
+  schema: "./db/schema.ts",
+  out: "./drizzle",
+  driver: "pg",
+  dbCredentials: {
+    connectionString: process.env.DATABASE_URL!,
+  },
+}
